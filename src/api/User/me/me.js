f5f2de6e76b662242e6e912dbdeb646e8a31bf14
inzahan <inzahanyang@gmail.com>
2020-04-17 13:53:47 +0900

    me fix

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -2,16 +2,10 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    me: async (_, __, { request, isAuthenticated }) => {
+    me: (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
       return prisma.user({ id: user.id });
-      // const userProfile = await prisma.user({ id: user.id });
-      // const posts = await prisma.user({ id: user.id }).posts();
-      // return {
-      //   user: userProfile,
-      //   posts
-      // };
-    }
-  }
+    },
+  },
 };
